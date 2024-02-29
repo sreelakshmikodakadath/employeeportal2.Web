4a02faa8bb04d3fea20093fac3c606f519d1619c
sreelakshmikodakadath <159633722+sreelakshmikodakadath@users.noreply.github.com>
2024-02-29 08:51:06 +0530

    Add files via upload

---
+++ Employeescontroller.cs
@@ -0,0 +1,34 @@
+﻿using EmployeePortal.Web.Data;
+using EmployeePortal.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeePortal.Web.Controllers
+{
+    public class Employeescontroller : Controller
+    {
+        public Employeescontroller(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel viewModel)
+        {
+            var employee = new EmployeePortal
+            {
+                Id = viewModel.Id,
+                Name = viewModel.Name,
+                Description = viewModel.Desription
+            };
+            await DbContext.Employees.AddAsync();
+            await DbContext.SaveChangesAsync();
+            return View();
+        }
+    }
+}
